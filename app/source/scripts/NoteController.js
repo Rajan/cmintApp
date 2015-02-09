d7c1e2bf6b407096b9141e334b0c74aec8339548
sudarsanps15 <sudarsan@classmint.com>
2015-02-09 14:38:44 +0530

    alert check on the controller

--- app/source/scripts/NoteController.js
+++ app/source/scripts/NoteController.js
@@ -3,4 +3,30 @@ angular
     .controller('NoteController', function($scope, supersonic) {
         $scope.navbarTitle = "";
         $cope.supersonic = supersonic;
+
+       $scope.init = function () {
+          /*  if ($routeParams.Id) {
+                //get an existing object
+            });
+        } else {
+            //create a new object
+
+        }
+
+        $scope.isSaving = false;*/
+       $window.alert('init');
+
+   var i=1;
+
+    };
+        //var jsonvalue  = document.getElementById("src").innerHTML;
+        //alert(jsonvalue);
+
+
     });
+/*if(i==1)
+
+{  supersonic.ui.dialog.alert("Custom title!");
+}*/
+
+init();
